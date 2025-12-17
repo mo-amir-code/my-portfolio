9fb249e08b54ccb168fb2a369797f9be29fb950c
Mo Amir <mo.amir.code@gmail.com>
2025-12-17 08:17:05 +0530

    refactor: image domain config

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["img.youtube.com", "res.cloudinary.com", "mekyu.cloud"],
+    domains: ["img.youtube.com", "res.cloudinary.com", "media.mekyu.cloud"],
   },
   // transpilePackages: ["next-mdx-remote"],
 };
